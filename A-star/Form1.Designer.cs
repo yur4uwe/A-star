1205ecf34eed6e0d1b28e2cea2a5e431ba903c6f
yur4uwe <tsg9515@gmail.com>
2024-06-01 15:42:47 +0300

    Add project files.

---
+++ A-star/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace A_star
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PathBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(26, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 800);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // PathBtn
+            // 
+            this.PathBtn.Location = new System.Drawing.Point(705, 8);
+            this.PathBtn.Name = "PathBtn";
+            this.PathBtn.Size = new System.Drawing.Size(75, 23);
+            this.PathBtn.TabIndex = 2;
+            this.PathBtn.Text = "Find Path";
+            this.PathBtn.UseVisualStyleBackColor = true;
+            this.PathBtn.Click += new System.EventHandler(this.PathBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 853);
+            this.Controls.Add(this.PathBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button PathBtn;
+    }
+}
---
+++ A-star/Form1.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace A_star
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap drawingBitmap;
+        private List<List<int>> map;
+        private int squares;
+        private int[] startEnd;
+
+        public const int SPACE = 0;
+        public const int OBSTACLE = 1;
+        public const int START = 2;
+        public const int END = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            drawingBitmap = new Bitmap(panel1.Width, panel1.Height);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try { squares = Convert.ToInt32(textBox1.Text); }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid number of squares input");
+                return;
+            }
+
+            map = new List<List<int>>(squares);
+
+            for (int i = 0; i < squares; ++i)
+            {
+                map.Add(new List<int>(squares));
+                for(int j = 0; j < squares; ++j)
+                {
+                    map[i].Add(SPACE);
+                }
+            }
+
+            DrawGrid();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "1";
+            squares = Convert.ToInt32(textBox1.Text);
+            startEnd = new int[4];
+
+            for(int i = 0; i < startEnd.Length; ++i)
+            {
+                startEnd[i] = -1;
+            }
+
+            map = new List<List<int>>(squares);
+
+            for (int i = 0; i < squares; ++i)
+            {
+                map.Add(new List<int>(squares));
+                for (int j = 0; j < squares; ++j)
+                {
+                    map[i].Add(SPACE);
+                }
+            }
+
+            this.Size = new System.Drawing.Size(800, 800);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e) { }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            panel1.Top = this.Height / 10;
+            panel1.Left = this.Width / 15;
+            panel1.Size = new System.Drawing.Size(
+                Math.Min(8 * this.Width / 10, 12 * this.Height / 15), 
+                Math.Min(8 * this.Width / 10, 12 * this.Height / 15)
+            );
+            
+            DrawGrid();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(drawingBitmap, Point.Empty);
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+
+            int square_l;
+            if (!int.TryParse(textBox1.Text, out square_l) || square_l <= 0)
+            {
+                MessageBox.Show("Please enter a valid positive integer in the text box.");
+                return;
+            }
+
+            int gridSize = panel1.Width / square_l;
+
+            int square_top = y / gridSize;
+            int square_left = x / gridSize;            
+
+            using (Graphics g = Graphics.FromImage(drawingBitmap))
+            {
+                Pen pen = new Pen(Color.Blue, 10);
+                Brush brush = new SolidBrush(Color.Red);
+
+                if(e.Button == MouseButtons.Left) 
+                {
+                    if (map[square_top][square_left] == OBSTACLE)
+                    {
+                        map[square_top][square_left] = SPACE;
+                        DrawGrid();
+                        return;
+                    }
+                    else if(map[square_top][square_left] == START)
+                    {
+                        startEnd[0] = startEnd[1] = -1;
+                    }
+                    else if(map[square_top][square_left] == END)
+                    {
+                        startEnd[2] = startEnd[3] = -1;
+                    }
+                    
+                    g.FillRectangle(brush, new Rectangle(
+                        new Point(gridSize * square_left, gridSize * square_top),
+                        new Size(gridSize, gridSize)
+                    ));
+
+                    map[square_top][square_left] = OBSTACLE;
+                }
+                else if(e.Button == MouseButtons.Right) 
+                {
+                    if (map[square_top][square_left] == OBSTACLE)
+                        return;
+                    Panel panel = new Panel();
+                    panel.Location = new Point(gridSize * square_left, gridSize * square_top);
+                    panel.Width = gridSize;
+                    panel.Height = gridSize;
+                    panel.MouseClick += new System.Windows.Forms.MouseEventHandler(Panel_MouseClick);
+                    panel.MouseLeave += new System.EventHandler(Panel_MouseLeave);
+
+                    Label instructions = new Label();
+                    instructions.Text = "LMouseCl for start\nRMouseCl for end";
+                    instructions.AutoSize = true;
+
+                    panel.Controls.Add(instructions);
+                    panel1.Controls.Add(panel);
+                }
+
+                pen.Dispose();
+                brush.Dispose();
+            }
+
+            panel1.Invalidate();
+        }
+
+        private void Panel_MouseClick(object sender, MouseEventArgs e)
+        {
+            int squareSize;
+            if (!int.TryParse(textBox1.Text, out squareSize) || squareSize <= 0)
+            {
+                MessageBox.Show("Please enter a valid positive integer in the text box.");
+                return;
+            }
+
+            Panel pnl = (Panel)sender;
+
+            int gridSize = panel1.Width / squareSize;
+            int row = pnl.Top / gridSize;
+            int col = pnl.Left / gridSize;
+
+            if (e.Button == MouseButtons.Left) // Set start point
+            {
+                if(startEnd[0] == row && startEnd[1] == col) //Erase start point if pressed twice 
+                {
+                    map[startEnd[0]][startEnd[1]] = SPACE;
+                    startEnd[0] = startEnd[1] = -1;
+                }
+                else
+                {
+                    if (startEnd[0] != -1) //check if we need to erase previous start point
+                    {
+                        map[startEnd[0]][startEnd[1]] = SPACE;
+                    }
+                    startEnd[0] = row;
+                    startEnd[1] = col;
+                    map[row][col] = START;
+                }
+            }
+            else if (e.Button == MouseButtons.Right) // Set end point
+            {
+                if (startEnd[2] == row && startEnd[3] == col) //Erase end point if pressed twice 
+                {
+                    map[startEnd[2]][startEnd[3]] = SPACE;
+                    startEnd[2] = startEnd[3] = -1;
+                }
+                else
+                {
+                    if (startEnd[2] != -1) //check if we need to erase previous end point
+                    {
+                        map[startEnd[2]][startEnd[3]] = 0;
+                    }
+                    startEnd[2] = row;
+                    startEnd[3] = col;
+                    map[row][col] = END;
+                }
+            }
+            
+            panel1.Controls.Clear();
+            DrawGrid();
+        }
+
+        private void Panel_MouseLeave(object sender, EventArgs e)
+        { panel1.Controls.Clear(); }
+
+        private void DrawGrid()
+        {
+            // Initialize a new bitmap to clear previous drawings
+            drawingBitmap = new Bitmap(panel1.Width, panel1.Height);
+
+            using (Graphics g = Graphics.FromImage(drawingBitmap))
+            {
+                Pen pen = new Pen(Color.Blue, 2); // Reduced pen width for grid lines
+                Brush brush = new SolidBrush(Color.Red);
+
+                int squares;
+                try
+                {
+                    squares = Convert.ToInt32(textBox1.Text);
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Invalid number of squares input");
+                    return;
+                }
+
+                int px_per_sq = panel1.Width / squares;
+
+                // Draw vertical and horizontal grid lines
+                for (int i = 0; i <= squares; ++i)
+                {
+                    g.DrawLine(pen, new Point(i * px_per_sq, 0), new Point(i * px_per_sq, panel1.Height));
+                    g.DrawLine(pen, new Point(0, i * px_per_sq), new Point(panel1.Width, i * px_per_sq));
+                }
+
+                // Fill the rectangles based on the map
+                for (int i = 0; i < squares; ++i)
+                {
+                    for (int j = 0; j < squares; ++j)
+                    {
+                        if (map[i][j] == 1)
+                        {
+                            g.FillRectangle(brush, new Rectangle(
+                                new Point(j * px_per_sq, i * px_per_sq), // Corrected the x, y coordinates
+                                new Size(px_per_sq, px_per_sq)
+                            ));
+                        }
+                    }
+                }
+
+                DrawStartEnd(startEnd[0], startEnd[1], Color.Green);
+                DrawStartEnd(startEnd[2], startEnd[3], Color.Red);
+
+                pen.Dispose();
+                brush.Dispose();
+            }
+
+            // Invalidate the panel to force a repaint
+            panel1.Invalidate();
+        }
+
+        private void DrawStartEnd(int row, int col, Color color)
+        {
+            using (Graphics g = Graphics.FromImage(drawingBitmap))
+            {
+                Brush brush = new SolidBrush(color);
+
+                int squares;
+                try
+                {
+                    squares = Convert.ToInt32(textBox1.Text);
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Invalid number of squares input");
+                    return;
+                }
+
+                int squareSize = panel1.Width / squares;
+                g.FillEllipse(brush, new Rectangle(
+                    new Point(col * squareSize + squareSize / 4, row * squareSize + squareSize / 4),
+                    new Size(squareSize / 2, squareSize / 2)
+                ));
+                brush.Dispose();
+            }
+
+            panel1.Invalidate();
+        }
+
+        public static void DrawCell(int x, int y)
+        {
+
+        }
+
+        private void PathBtn_Click(object sender, EventArgs e)
+        {
+            PathFinder pathFinder = new PathFinder();
+
+            pathFinder.FindPath(map, startEnd);
+        }
+    }
+
+    class PathFinder
+    {
+        private List<int[]> q;
+        private List<List<bool>> visited;
+
+
+        public PathFinder() 
+        {
+            q = new List<int[]>();
+            visited = new List<List<bool>>();
+        }
+
+        public void FindPath(List<List<int>> map, int[] startEnd) //first will be BFS
+        {
+            visited = new List<List<bool>>(map.Count);
+            for (int i = 0; i < map.Count; i++)
+            {
+                visited.Add(new List<bool>(map[0].Count));
+                for (int j = 0; j < map[0].Count; j++)
+                {
+                    visited[i].Add(false);
+                }
+            }
+
+            q.Add(new int[2] { startEnd[0], startEnd[1] });
+            visited[startEnd[0]][startEnd[1]] = true;
+
+            while(q.Count > 0)
+            {
+                for (int i = 0; i < q.Count; i++)
+                {
+                    int[] currCell = q[i];
+                    Form1.DrawCell(currCell[0], currCell[1]);
+
+                    if (currCell[0] > 0 && map[currCell[0] - 1][currCell[1]] != Form1.OBSTACLE)
+                    {
+                        q.Add(new int[2] { currCell[0] - 1, currCell[1] });
+                        visited[currCell[0] - 1][currCell[1]] = true;
+                    }
+                    if (currCell[0] < map.Count - 1 && map[currCell[0] + 1][currCell[1]] != Form1.OBSTACLE)
+                    {
+                        q.Add(new int[2] { currCell[0] + 1, currCell[1] });
+                        visited[currCell[0] + 1][currCell[1]] = true;
+                    }
+                    if (currCell[1] > 0 && map[currCell[0]][currCell[1] - 1] != Form1.OBSTACLE)
+                    {
+                        q.Add(new int[2] { currCell[0], currCell[1] - 1 });
+                        visited[currCell[0]][currCell[1] - 1] = true;
+                    }
+                    if (currCell[1] < map[0].Count - 1 && map[currCell[0]][currCell[1] + 1] != Form1.OBSTACLE)
+                    {
+                        q.Add(new int[2] { currCell[0], currCell[1] + 1 });
+                        visited[currCell[0]][currCell[1] + 1] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new Form1());
+        }
+    }
+}
